d8c0559c34058c0ef65b72f72d8c8a8535f3e27e
Murat Ertik <dmuratertik1@gmail.com>
2024-04-17 19:14:46 +0300

    the queries file has been created .

---
+++ Queries.sql
@@ -0,0 +1,5 @@
+use flights_ticket_project;
+insert into airports (airportsid,airport_name,country,city,adress)
+VALUES ('SBH','Sabiha Gokcen Airport','Turkiye','Istanbul','Sanayi, 34906 Pendik/İstanbul'),
+('ESB','Esenboga Airport','Turkiye','Ankara','Balıkhisar St., Ozal Avenue, Akyurt/Ankara'),
+('JFK','John F. Kennedy Airport','USA','New York','Queens, New York 11430, USA');
