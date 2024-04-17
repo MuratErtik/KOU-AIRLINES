a384ab54cc5eb5c7031e59b7d8006b07f55119f1
Murat Ertik <dmuratertik1@gmail.com>
2024-04-17 19:54:46 +0300

    new queries have been added.

--- Queries.sql
+++ Queries.sql
@@ -3,3 +3,5 @@ insert into airports (airportsid,airport_name,country,city,adress)
 VALUES ('SBH','Sabiha Gokcen Airport','Turkiye','Istanbul','Sanayi, 34906 Pendik/İstanbul'),
 ('ESB','Esenboga Airport','Turkiye','Ankara','Balıkhisar St., Ozal Avenue, Akyurt/Ankara'),
 ('JFK','John F. Kennedy Airport','USA','New York','Queens, New York 11430, USA');
+-------------------------------------------------------------------------------------------------------
+select airport_name,airportsid from airports
