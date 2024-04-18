5084ae505ea304b6ac2d1060bf32691e0461c12e
Murat Ertik <dmuratertik1@gmail.com>
2024-04-18 15:00:40 +0300

    The data which pull from MYSQL have been added in Autocomplete inputs.

--- Queries.sql
+++ Queries.sql
@@ -2,6 +2,18 @@ use flights_ticket_project;
 insert into airports (airportsid,airport_name,country,city,adress)
 VALUES ('SBH','Sabiha Gokcen Airport','Turkiye','Istanbul','Sanayi, 34906 Pendik/İstanbul'),
 ('ESB','Esenboga Airport','Turkiye','Ankara','Balıkhisar St., Ozal Avenue, Akyurt/Ankara'),
-('JFK','John F. Kennedy Airport','USA','New York','Queens, New York 11430, USA');
+('JFK','John F. Kennedy Airport','USA','New York','Queens, New York 11430, USA'),
+('ADB','Izmir Adnan Menderes Airport','Turkiye','Izmir','Dokuz Eylul, 35410 Gaziemir/Izmir'),
+('EZS','Elazig Airport','Turkiye','Elazig','Yesilyurt St. Elazig Airport, Havalimani St., 23000 Akcakiraz/Elazig'),
+('YEI','Bursa Yenisehir Airport','Turkiye','Bursa','Cardak, 16900 Yenisehir/Bursa'),
+('RZV','Rize Artvin Airport','Turkiye','Rize','Rize-Artvin Airport, 53300 Pazar/Rize'),
+('DIY','Diyarbakir Airport','Turkiye','Diyarbakir','Kamisli Avenue, 21090 Baglar/Diyarbakir'),
+('BJV','Milas-Bodrum Airport','Turkiye','Mugla','Ekinanbari, Havalimanı St, 48200 Milas/Mugla'),
+('AYT','Antalya Airport','Turkiye','Antalya','Yesilkoy, 07230 Muratpasa/Antalya'),
+('SOF','Sofia Airport','Bulgaria','Sofia','1 Chistophor Columbus Blvd, Sofia Airport EAD, 1540 Sofia, Bulgaria'),
+('BER','Berlin Brandenburg Airport','Germany','Berlin','Melli-Beese-Ring 1, 12529 Schönefeld, Germany'),
+('ROM','Roma Airport','Italy','Roma','Via L.Da Vinci, 00015 Roma RM, İtaly'),
+('CDG','Charles De Gaulle Airport','France','Paris','95700 Roissy-en-France, France'),
+('NRT','Tokyo Airport','Japan','Tokyo','1-1 Furugome, Narita, Chiba 282-0004, Japan');
 -------------------------------------------------------------------------------------------------------
 select airport_name,airportsid from airports
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+// CORS settings
+app.use(cors());
+
+
+
+
+// MySQL connection information
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'flights_ticket_project'
+});
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error('While connect to MYSQL database the error occurred: ' + err.stack);
+        return;
+    }
+    console.log('Connect the successfully.');
+});
+
+
+app.get('/users', (req, res) => {
+    
+    const sqlQuery = 'select airport_name,airportsid from airports';
+
+    
+    connection.query(sqlQuery, (error, results) => {
+        if (error) {
+            console.error('Query Error: ' + error);
+            res.status(500).send('Database Error');
+            return;
+        }
+        
+        //result of queries send client via JSON
+        res.json(results);
+    });
+});
+
+
+//Execute the Server
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`The Server execute at ${port} port.`);
+});
