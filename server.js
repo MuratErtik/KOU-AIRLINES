1f8a954cd2afbc8848207870eaca1069477455d6
Murat Ertik <dmuratertik1@gmail.com>
2024-04-23 18:41:09 +0300

    Card Info has been changed.

--- server.js
+++ server.js
@@ -29,34 +29,34 @@ connection.connect((err) => {
 
 
 app.get('/users', (req, res) => {
-    
+
     const sqlQuery1 = 'select airport_name,airportsid from airports';
 
-    
+
     connection.query(sqlQuery1, (error, results) => {
         if (error) {
             console.error('Query Error for query1: ' + error);
             res.status(500).send('Database Error');
             return;
         }
-        
+
         //result of queries send client via JSON
         res.json(results);
     });
 });
 
 app.get('/citizenship', (req, res) => {
-    
+
     const sqlQuery1 = 'select citizenship from citizenship';
 
-    
+
     connection.query(sqlQuery1, (error, results) => {
         if (error) {
             console.error('Query Error for query1: ' + error);
             res.status(500).send('Database Error');
             return;
         }
-        
+
         res.json(results);
     });
 });
@@ -67,24 +67,40 @@ app.get('/flights', (req, res) => {
 
     // Construct the SQL query based on user input
     const sqlQuery = `
-        SELECT flights.from, flights.to, flights.flight_time, flights.departure_date, flights.flight_price
-        FROM flights 
-        WHERE flights.from= ? AND flights.to=?
-        
-    `;
+    SELECT 
+    airports.airport_name AS from_airport_name, 
+    airports_to.airport_name AS to_airport_name, 
+    flights.flight_time, 
+    flights.departure_date, 
+    flights.flight_price,
+    airports.city AS from_airport_city,
+    airports_to.city AS to_airport_city
+FROM 
+    flights
+INNER JOIN 
+    airports ON flights.from = airports.airportsid
+INNER JOIN 
+    airports AS airports_to ON flights.to = airports_to.airportsid
+WHERE 
+    flights.from = ? 
+    AND flights.to = ?;
+
+`;
+
+
 
     // Execute the SQL query with user input as parameters
-    connection.query(sqlQuery,[selectedFrom,selectedTo], (error, results) => {
+    connection.query(sqlQuery, [selectedFrom, selectedTo], (error, results) => {
         if (error) {
             console.error('Error executing SQL query:', error);
             res.status(500).send('Database Error');
             return;
         }
-        
+
         // Check if any flights match the criteria
         if (results.length === 0) {
             res.status(404).send('No flights found matching the criteria.');
-            
+
         } else {
             // Send the results back to the client in JSON format
             res.json(results);
