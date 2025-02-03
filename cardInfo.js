8e04c1ef83c829d68157af83aca2d2ff5a2e19bf
Murat Ertik <dmuratertik1@gmail.com>
2025-02-02 23:59:33 -0800

    v2

--- cardInfo.js
+++ cardInfo.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     for (const el of document.querySelectorAll("[placeholder][data-slots]")) {
+
+
         const pattern = el.getAttribute("placeholder"),
             slots = new Set(el.dataset.slots || "_"),
             prev = (j => Array.from(pattern, (c, i) => slots.has(c) ? j = i + 1 : j))(0),
@@ -12,22 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 );
             },
             format = () => {
+
                 const [i, j] = [el.selectionStart, el.selectionEnd].map(i => {
                     i = clean(el.value.slice(0, i)).findIndex(c => slots.has(c));
                     return i < 0 ? prev[prev.length - 1] : back ? prev[i - 1] || first : i;
                 }); el.value = clean(el.value).join``; el.setSelectionRange(i, j); back = false;
             }; let back = false; el.addEventListener("keydown", (e) => back = e.key === "Backspace");
+
         el.addEventListener("input", format);
         el.addEventListener("focus", format);
         el.addEventListener("blur", () => el.value === pattern && (el.value = ""));
     }
 });
 
+
 var user_name = document.querySelector(".user_name");
 var set_card_number = document.querySelector(".set_card_number");
 var user_cardcvv = document.querySelector(".user_card_cvv")
 var user_name_input = document.getElementById("user_name_input");
 var user_card_number_input = document.getElementById("user_card_number_input");
+
 function userName(name) {
 
     user_name.innerHTML = name;
@@ -48,6 +55,7 @@ var right_side = document.querySelector(".right-side");
 var success = document.querySelector(".success");
 
 
+
 function validateform() {
     validate = true;
     var validate_inputs = document.querySelectorAll(".right-side input");
@@ -61,6 +69,7 @@ function validateform() {
             }
         }
     });
+    
     return validate;
 }
 
